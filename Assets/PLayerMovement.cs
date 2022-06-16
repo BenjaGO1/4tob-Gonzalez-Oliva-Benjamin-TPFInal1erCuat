eba6ad87bc5e1a591a321ae86b1854e4ca77eee1
BenjaGO1 <101335426+BenjaGO1@users.noreply.github.com>
2022-06-15 22:10:17 -0300

    Initial commit

---
+++ Assets/PLayerMovement.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PLayerMovement : MonoBehaviour
+{
+
+    public float movementSpeed;
+    public float rotationSpeed;
+    public float jumpForce;
+    public int maxJumps;
+
+    int hasJump;
+    Rigidbody rb;
+
+    void Start()
+    {
+        hasJump = maxJumps;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(0, 0, movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(0, 0, -movementSpeed);
+        }
+        if (Input.GetKey(KeyCode.D) && hasJump==maxJumps)
+        {
+            transform.Rotate(0, rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.A) && hasJump == maxJumps)
+        {
+            transform.Rotate(0, -rotationSpeed, 0);
+        }
+        if (Input.GetKey(KeyCode.Space) && hasJump>0)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            hasJump--;
+        }
+    }
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.gameObject.name == "ground")
+        {
+            hasJump = maxJumps;
+        }
+    }
+}
---
+++ Assets/bullet.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public float LifeTime;
+    //public ParticleSystem DestroyBullet;
+    public GameObject enemy;
+    public GameObject enemy2;
+    public GameObject enemy3;
+    public int cantenemy;
+    public int cantenemy2;
+    public int cantenemy3;
+
+    void Start()
+    {
+        Destroy(gameObject, LifeTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.right * speed * Time.deltaTime;
+    }
+
+    private void OnDestroy()
+    {
+        //Instantiate(DestroyBullet, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "enemy")
+        {
+            Destroy(enemy);
+        }
+    }
+}
